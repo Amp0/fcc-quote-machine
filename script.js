3baf434a575e0a0a60747d08fd3b87b3d43f0016
Ampo0 <amedeefleau@gmail.com>
2016-11-16 14:52:45 +0100

    Added API call

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#77B1A9", "#73A857"];
 
 $(document).ready(function() {
-
+  $.ajaxSetup({cache:false});
+  var actualColor = -1;
   // Define color changing function
   function changeColor() {
-    var color = colors[Math.floor(Math.random()*colors.length)];
-
+    var color = actualColor;
+    while(actualColor == color) {
+      color = colors[Math.floor(Math.random()*colors.length)];
+    }
     $('h1').css('color', color);
     $('body').css('background-color', color);
     $('#quote-wrap').css('color', color);
@@ -14,9 +17,11 @@ $(document).ready(function() {
 
   $('#btn-another').click(function () {
     // Get Quote
-
-
-    // Change color
-    changeColor();
+    var data;
+    $.getJSON("https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(data){
+      $('#quote').html("<i class='fa fa-quote-left'></i> "+ data.quoteText +" <i class='fa fa-quote-right'>");
+      $('#quote-author').text(data.quoteAuthor);
+      changeColor();
+    });
   });
 });
